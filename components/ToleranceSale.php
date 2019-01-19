b0a657c9fffb9279d91f511e000112f181f1bfb6
RAPOS <hostmas@bk.ru>
2019-01-19 20:11:20 +0500

    Test

--- components/ToleranceSale.php
+++ components/ToleranceSale.php
@@ -23,12 +23,17 @@ class ToleranceSale extends Component
      * $Tolerance Допустимое отклонение в %
      * $CurrentPrice Текущая цена
      * $PreviousPrice Предыдущая цена
+     *
+     * В случае, если результат будет больше, чем допустимое отклонение, то возвращаем false
      */
     public function diff($Tolerance, $CurrentPrice, $PreviousPrice = 0)
     {
         $this->Result = (($PreviousPrice - $CurrentPrice) / $CurrentPrice) * 100;
+
+        return ($this->Result > $Tolerance) ? false : true;
     }
 
+    //Результ вычисления
     public function getAmount()
     {
         return round($this->Result);
